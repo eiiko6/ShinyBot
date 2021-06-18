2e13a75714cf19d11adbd03b4fa2c4a5b666b9d7
mxstoto6 <85441122+mxstoto6@users.noreply.github.com>
2021-06-18 08:05:28 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -2,13 +2,13 @@ const Discord = require ("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Tu n'as pas la permission d'utiliser cette commande");
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You don't have the permission to use this command.");
     if (!args[0]) return message.channel.send("Indiques un nombre");
-    if (args[0] < 1 || args[0] > 100) return message.channel.send("Veuillez indiquer un nombre entre 1 et 100")
+    if (args[0] < 1 || args[0] > 100) return message.channel.send("Please, enter a number between 0 and 100.")
 
     message.delete()
     message.channel.bulkDelete(args[0]).then(() => {
-        message.channel.send(`${args[0]} messages ont été supprimés`).then(msg => msg.delete(5000))
+        message.channel.send(`${args[0]} messages was deleted !`).then(msg => msg.delete(5000))
 
         var today = new Date();
         var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();

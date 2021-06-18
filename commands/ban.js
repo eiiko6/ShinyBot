649ce937d28b7fe9e7cc1903c73803aa812d8d89
mxstoto6 <85441122+mxstoto6@users.noreply.github.com>
2021-06-18 08:03:21 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
  
 module.exports.run = async (bot, message, args) => {
-    if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send(":x: Vous n'avez pas la permission d'utiliser cette commande ;(")
+    if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send(":x: You don't have the permission to execute this command ;(")
        let member = message.mentions.members.first()
-       if (!member) return message.channel.send(":x: Veuillez mentionner un utilisateur")
-       if (!member.bannable) return message.channel.send(":x: Je ne peut pas bannir cet utilisateur. :x:")
+       if (!member) return message.channel.send(":x: Please, mentions an user.")
+       if (!member.bannable) return message.channel.send(":x:  can't ban this one :x:")
        message.guild.ban(member, {days: 7})
        message.delete();
-       message.channel.send('**' + member.user.username + '** a été banni :white_check_mark:')
+       message.channel.send('**' + member.user.username + '** was banned succesfully :white_check_mark:')
 
        var today = new Date();
        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();

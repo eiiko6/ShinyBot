f7426dd1969c07e5be4facb0860586741b2c44aa
mxstoto6 <85441122+mxstoto6@users.noreply.github.com>
2021-06-18 16:55:05 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
     if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send(":x: You don't have the permission to execute this command ;(")
        let member = message.mentions.members.first()
        if (!member) return message.channel.send(":x: Please, mentions an user.")
-       if (!member.bannable) return message.channel.send(":x:  can't ban this one :x:")
+       if (!member.bannable) return message.channel.send(":x: I can't ban this one :x:")
        message.guild.ban(member, {days: 7})
        message.delete();
        message.channel.send('**' + member.user.username + '** was banned succesfully :white_check_mark:')

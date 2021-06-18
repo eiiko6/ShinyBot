5c69dbd9c25076f65f5f4cf8e82117a3fb710119
mxstoto6 <85441122+mxstoto6@users.noreply.github.com>
2021-06-18 16:55:51 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,13 +1,13 @@
 const Discord = require("discord.js");
  
 module.exports.run = async (bot, message, args) => {
-    if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send(":x: Vous n'avez pas la permission d'utiliser cette commande ;(")
+    if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send(":x: You don't have the permission to execute this command ;(")
        let member = message.mentions.members.first()
-       if (!member) return message.channel.send(":x: Veuillez mentionner un utilisateur")
-       if (!member.bannable) return message.channel.send(":x: Je ne peut pas exclure cet utilisateur.")
+       if (!member) return message.channel.send(":x: Please, mentions an user.")
+       if (!member.bannable) return message.channel.send(":x: I can't ban this one.")
        message.guild.ban(member, {days: 7})
        message.delete();
-       message.channel.send('**' + member.user.username + '** a été exclu :white_check_mark:')
+       message.channel.send('**' + member.user.username + '** was kicked from the server :white_check_mark:')
 
        var today = new Date();
        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();

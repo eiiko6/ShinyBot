fabd89d6212b9f651588a7e5b736c57e9e832a01
mxstoto6 <85441122+mxstoto6@users.noreply.github.com>
2021-06-18 08:14:36 +0200

    Update guide.js

--- commands/guide.js
+++ commands/guide.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
 
   message.delete()
 
-  message.channel.send("Voici le lien vers le guide de ShinyBot :\nhttps://bit.ly/Guide-ShinyBot")
+  message.channel.send("Link to the ShinyBot's guide :\nhttps://bit.ly/Guide-ShinyBot")
 
   var today = new Date();
   var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();

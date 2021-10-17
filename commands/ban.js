032d5df9e1c9f182b378104248945284e4cd868b
mxstoto <85441122+mxstoto6@users.noreply.github.com>
2021-10-17 11:40:16 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -2,10 +2,13 @@ const Discord = require("discord.js");
  
 module.exports.run = async (bot, message, args) => {
     if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send(":x: You don't have the permission to execute this command ;(")
+       const args = message.content.slice(prefix.length).trim().split(/ +/g);
+       const command = args.shift().toLowerCase();
+       let days = args.slice(1).join(" ");
        let member = message.mentions.members.first()
        if (!member) return message.channel.send(":x: Please, mentions an user.")
        if (!member.bannable) return message.channel.send(":x: I can't ban this one.")
-       message.guild.ban(member, {days: 7})
+       message.guild.ban(member, {days: days})
        message.delete();
        message.channel.send('**' + member.user.username + '** was banned succesfully :white_check_mark:')
 
